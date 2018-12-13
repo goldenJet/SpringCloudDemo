e97b7a9a90cef6f034715af2ee05dfe8d7c981a2
jet.chen <505214992@qq.com>
2018-12-13 23:49:25 +0800

    eureka 高可用

--- 1-EurekaDemo/EurekaDemo/eurekaclient/src/main/java/com/goldenjet/EurekaClientApplication.java
+++ 1-EurekaDemo/EurekaDemo/eurekaclient/src/main/java/com/goldenjet/EurekaClientApplication.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @EnableEurekaClient
+//@EnableDiscoveryClient
 @SpringBootApplication
 @RestController
 public class EurekaClientApplication {
